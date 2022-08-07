2d8ef04e57ab4a334ecfab76d73ec457b9148bc5
Irenitemi Agbejule <47606100+Dart120@users.noreply.github.com>
2022-08-07 12:13:36 +0100

    Auto yes for package install in build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 conan profile update settings.compiler.libcxx=libstdc++11 default
 export CONAN_SYSREQUIRES_MODE=enabled
-apt install pkg-config
-apt install cmake
+apt install -y pkg-config
+apt install -y cmake
 conan install . --build=missing -c tools.system.package_manager:mode=install
 cmake .
 make
