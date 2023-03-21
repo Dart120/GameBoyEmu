8a7f5831b6e6f2382cae9455ab985f0d154c2001
Dart.120 <irenitemi81@gmail.com>
2023-03-21 16:57:17 +0000

    Finished 0xC0-CF fixed bug

--- src/MATH/MATH.cpp
+++ src/MATH/MATH.cpp
@@ -19,6 +19,14 @@ void CPU::ADD_1B_1C(uint8_t reg1, uint32_t *cycles){
     this->registers.registers.PC++;
 }
 void CPU::ADC_1B_1C(uint8_t reg1, uint32_t *cycles){
+    this->registers.check_H_8_ADD(this->registers.registers.AF.A,(uint8_t)  (reg1 + this->registers.get_flag(FLAG_C)));
+    this->registers.check_C_8_ADD(this->registers.registers.AF.A,(uint8_t)  (reg1 + this->registers.get_flag(FLAG_C)));
+    this->registers.registers.AF.A += (uint8_t) (reg1 + this->registers.get_flag(FLAG_C));
+    this->registers.check_if_result_zero(this->registers.registers.AF.A);
+    *cycles--;
+    this->registers.registers.PC++;
+}
+void CPU::ADC_2B_2C(uint32_t *cycles){
     uint8_t d8 = this->memory.read_8_bit(this->registers.registers.PC + 1);
     this->registers.check_H_8_ADD(this->registers.registers.AF.A,(uint8_t)  (d8 + this->registers.get_flag(FLAG_C)));
     this->registers.check_C_8_ADD(this->registers.registers.AF.A,(uint8_t)  (d8 + this->registers.get_flag(FLAG_C)));
@@ -26,14 +34,6 @@ void CPU::ADC_1B_1C(uint8_t reg1, uint32_t *cycles){
     this->registers.check_if_result_zero(this->registers.registers.AF.A);
     *cycles -= 2;
     this->registers.registers.PC += 2;
-}
-void CPU::ADC_2B_2C(uint32_t *cycles){
-    this->registers.check_H_8_ADD(this->registers.registers.AF.A,(uint8_t)  (reg1 + this->registers.get_flag(FLAG_C)));
-    this->registers.check_C_8_ADD(this->registers.registers.AF.A,(uint8_t)  (reg1 + this->registers.get_flag(FLAG_C)));
-    this->registers.registers.AF.A += (uint8_t) (reg1 + this->registers.get_flag(FLAG_C));
-    this->registers.check_if_result_zero(this->registers.registers.AF.A);
-    *cycles--;
-    this->registers.registers.PC++;
 }
 
 void CPU::ADD_1B_2C_8Bit(uint8_t reg1, uint32_t *cycles){
